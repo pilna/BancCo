3adbc4bdf36b06c6259df64a692347b400a6ffc3
Bernagaud Tristan <tristan.bernagaud@etu.univ-orleans.fr>
2023-01-04 23:51:15 +0100

    Avancer profile

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,13 +1,155 @@
 import { MobileLayout } from '../../layout'
 import React from 'react'
-import { Text } from 'react-native'
+import {Image, ImageBackground, Text, Button, View, Alert} from 'react-native'
+import {styles} from './profile.style'
+import {ButtonFactory} from "../../components";
+
+const buttonFactory = new ButtonFactory();
 
 const ProfilePage = ({ navigation }) => {
   return (
-    <MobileLayout navigation={navigation}>
-      <Text>Profile Page</Text>
-    </MobileLayout>
+      <MobileLayout navigation={navigation}>
+      <View style={styles.profilePageContainer}>
+          <ImageBackground source={require ('../../../assets/background.png')} style={styles.backgroundImage} >
+
+              <View style={styles.topContentContainer}>
+
+                  <Text style={{
+                  ...styles.colorizedText,
+                  ...styles.profileTitle
+
+                  }}>
+                    Profile
+                  </Text>
+
+                  <Image source={require ('../../../assets/favicon.png')} style={styles.profileImage}/>
+
+
+                      <View style={ styles.sugdeg}>
+                          <View style={styles.sugdegflex}>
+                              <Text>0</Text>
+                          </View>
+                          <View style={styles.sugdegflex}>
+                              <Text>0</Text>
+                          </View>
+                      </View>
+                      <View style={ styles.sugdeg}>
+                          <View style={styles.sugdegflex}>
+                              <Text>Dégradation</Text>
+                          </View>
+                          <View style={styles.sugdegflex}>
+                              <Text>Suggestion</Text>
+                          </View>
+                      </View>
+
+
+                  <View style={styles.profileInputContainer}>
+                      <Text style={styles.profileInputLabel}>Statistique  </Text>
+                  <View style={styles.back}>
+
+                      <Button
+                          onPress={() => Alert.alert('Not link')}
+                          color={'#000000'}
+                          title="Your Trails"
+                      />
+                      <Button
+                          onPress={() => Alert.alert('Not link')}
+                          color={'#000000'}
+                          title="Saved trails"
+                      />
+                  </View>
+                      <Text style={styles.profileInputLabel}>Lists </Text>
+                      <View style={styles.back}>
+
+                          <Button
+                              onPress={() => Alert.alert('Not link')}
+                              color={'#000000'}
+                              title="Favorites"
+                          />
+                          <Button
+                              onPress={() => Alert.alert('Not link')}
+                              color={'#000000'}
+                              title="Antalya"
+                          />
+                          <Button
+                              onPress={() => Alert.alert('Not link')}
+                              color={'#000000'}
+                              title="Lycan Way"
+                          />
+
+                      </View>
+                  </View>
+
+              </View>
+          </ImageBackground>
+      </View>
+      </MobileLayout>
+
+
   )
 }
 
+
+/*
+
+
+const LoginPage = ({ navigation }) => {
+
+  return (
+    <View style={styles.loginPageContainer}>
+      <View style={styles.topContentContainer}>
+        <Text style={{
+          ...styles.colorizedText,
+          ...styles.loginTitle
+        }}>
+          Banc.co
+        </Text>
+
+        <Text style={{
+          ...styles.colorizedText,
+          ...styles.loginTitle
+        }}>
+          Log In
+        </Text>
+
+        <View style={styles.loginInputContainer}>
+          <Text style={styles.loginInputLabel}>
+            E-mail or username
+          </Text>
+
+          <TextInput
+            onChangeText={setEmail}
+            style={styles.loginInputBox}
+            value={email}
+            placeholder="Your E-mail or username"
+          />
+        </View>
+
+        <View style={styles.loginInputContainer}>
+          <Text style={styles.loginInputLabel}>
+            Password
+          </Text>
+
+          <TextInput
+            style={styles.loginInputBox}
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Your Password"
+            secureTextEntry
+          />
+        </View>
+      </View>
+
+      {buttonFactory.createSubmitButton(
+        "Sign In",
+        () => {
+          onLogin();
+          navigation.navigate(routes.explore);
+        }
+      )}
+    </View>
+  )
+}
+
+export default LoginPage*/
 export default ProfilePage
---
+++ src/pages/profile/profile.style.js
@@ -0,0 +1,67 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    profilePageContainer: {
+        paddingHorizontal: "5%",
+        paddingVertical: "5%",
+        height: "100%",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    profileTitle: {
+        marginTop: 10,
+        fontSize: 24,
+    },
+    profileInputContainer: {
+        width: "100%",
+        alignItems: "center",
+        marginTop: 90,
+    },
+    profileInputLabel: {
+        fontSize: 20,
+        marginBottom: 4,
+
+    },
+    profileInputBox: {
+        fontSize: 20,
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        border: "1px solid #777777",
+        borderRadius: 4,
+    },
+    colorizedText: {
+        color: "#4C8C2C",
+    },
+    topContentContainer: {
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    backgroundImage: {
+        height: '100%',
+        width: '120%',//c'est moche à changer mais ca marche
+    },
+    profileImage: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+        width: 100,
+        height: 100
+    },
+    sugdeg:{
+        flexDirection: 'row',
+    },
+    sugdegflex:{
+
+        alignItems: "center",
+        marginTop: 10,
+        flex: 1
+    },
+    back:{
+        width: '80%',
+        backgroundColor: "#fff",
+
+    }
+
+});
