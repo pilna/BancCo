11dd3a0c3c6f1b6eda81ba6b302d74b806efabc2
pilna <pilna@outlook.fr>
2023-01-04 16:58:41 +0100

    add icon

--- src/components/navigation/navigation-button/navigation-button.js
+++ src/components/navigation/navigation-button/navigation-button.js
@@ -4,16 +4,12 @@ import React from 'react'
 import { styles } from './navigation-button.style'
 
 const NavigationButton = ({ navigation, item }) => {
-  console.log(item)
   return (
     <View 
       style={styles.buttonContainer}
       onTouchEnd={() => navigation.navigate(item.to)}
     >
-      <Image 
-        style={styles.buttonIcon}
-        source={item.icon} 
-      />
+      {item.icon}
       <Text style={styles.buttonText}>
         {item.label}
       </Text>
--- src/components/navigation/navigation.config.js
+++ src/components/navigation/navigation.config.js
@@ -1,28 +1,29 @@
-import arrowUpCircle from "../../../assets/arrow-up-circle.svg";
-import earth from "../../../assets/earth.svg";
-import map from "../../../assets/map.svg";
-import profile from "../../../assets/profile.svg";
+import ArrowUpCircle from "../../../assets/arrow-up-circle.svg";
+import Earth from "../../../assets/earth.svg";
+import Map from "../../../assets/map.svg";
+import Profile from "../../../assets/profile.svg";
+import { SvgXml } from 'react-native-svg';
 import { routes } from "../../router";
 
 export const navigationConfig = {
   items: [
     {
-      icon: earth,
+      icon: <Earth width={36} height={36} />,
       label: "Explore",
       to: routes.explore
     },
     {
-      icon: arrowUpCircle,
+      icon: <ArrowUpCircle width={36} height={36} />,
       label: "Record Trail",
       to: routes.recordTrail
     },
     {
-      icon: map,
+      icon: <Map width={36} height={36} />,
       label: "Promoted Trail",
       to: routes.promotedTrail
     },
     {
-      icon: profile,
+      icon: <Profile width={36} height={36} />,
       label: "Profile",
       to: routes.profile
     }
