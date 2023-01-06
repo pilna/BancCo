17bcd83b4f63e78345858f0dfef525e364aa3639
pilna <pilna@outlook.fr>
2023-01-06 15:46:13 +0100

    fix map orléans

--- src/pages/explore/explore.js
+++ src/pages/explore/explore.js
@@ -7,13 +7,13 @@ const ExplorePage = ({ navigation }) => {
   const { loading, error, suggestions } = useSuggestions();
 
   // console.log("suggestions", suggestions);
-  
+
   return (
     <MobileLayout navigation={navigation}>
       <MapView 
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 47.9027336,
+          longitude: 1.9086066,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
