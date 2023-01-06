dc6fdf381a748a6a0bb7cbacaee069602ea5f6eb
Bernagaud Tristan <tristan.bernagaud@etu.univ-orleans.fr>
2023-01-06 23:39:52 +0100

    itineraire v1

--- src/pages/explore/explore.js
+++ src/pages/explore/explore.js
@@ -2,15 +2,68 @@ import MapView, { Marker } from 'react-native-maps'
 
 import { MobileLayout } from '../../layout'
 import { PavModal } from '../../components';
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { usePav } from '../../hooks/usePav';
 import { useSelectedPav } from '../../hooks/useSelectedPav';
+import MapViewDirections from "react-native-maps-directions";
+import * as Location from 'expo-location';
+
 
 const ExplorePage = ({ navigation }) => {
   const { loading, error, pav } = usePav();
   const { selectedPav, selectPav } = useSelectedPav();
 
-  return (
+
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+    console.log("text", text)
+
+
+    let moche1;
+    let moche2;
+    if(location == null){
+        moche1 = 45.521563;
+        moche2 = 1.9086066;
+    }else {
+        moche1 = location.coords.latitude;
+        moche2 = location.coords.longitude;
+    }
+
+    const origin = {
+        latitude: moche1,
+        longitude: moche2,
+    };
+    const destination = {
+        latitude: 47.9027336,
+        longitude: 1.9086066,};
+    const GOOGLE_MAPS_APIKEY = 'AIzaSyBlhYWOi17iTeIgISCWBxtCTjdufXscqdM';
+
+
+
+
+
+    return (
     <MobileLayout navigation={navigation}>
       <MapView 
         initialRegion={{
@@ -23,6 +76,12 @@ const ExplorePage = ({ navigation }) => {
           width: '100%',
           height: '100%'
         }}
+
+        showsUserLocation
+        showsMyLocationButton
+        showsCompass
+        showsScale
+        loadingEnabled
       >
         {pav && pav.map((item, index) => (
           <Marker
@@ -37,6 +96,17 @@ const ExplorePage = ({ navigation }) => {
             }}
           />
         ))}
+
+          <Marker coordinate={origin} />
+          <Marker coordinate={destination} />
+          <MapViewDirections
+              origin={origin}
+              destination={destination}
+              apikey={GOOGLE_MAPS_APIKEY}
+              strokeWidth={3}
+              strokeColor="hotpink"
+          />
+
       </MapView>
 
       {selectedPav && (
